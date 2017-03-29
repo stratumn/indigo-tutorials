c2d4f20e2b952e33713a2d6278111de15203eda6
Stephan Florquin <stephan.florquin@gmail.com>
2017-03-29 15:14:57 +0200

    create branch for part 1

--- part2/scripts/lib/genkey.js
+++
@@ -1,9 +0,0 @@
-var bitcore = require('bitcore-lib');
-
-var privateKey = new bitcore.PrivateKey();
-var publicKey = bitcore.PublicKey(privateKey);
-var address = privateKey.toAddress();
-
-console.log('Address:\t', address.toString());
-console.log('Public key:\t', publicKey.toString());
-console.log('Private key:\t', privateKey.toString());
