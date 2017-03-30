846ac213edfa58090364dfd50c66aa41d017f604
Stephan Florquin <stephan.florquin@gmail.com>
2017-03-31 01:26:59 +0200

    add comments to sign script

--- scripts/lib/sign.js
+++ scripts/lib/sign.js
@@ -2,10 +2,14 @@ var readline = require('readline');
 var bitcore = require('bitcore-lib');
 var Message = require('bitcore-message');
 
+// Readline is a builtin node module to read a line from the user.
 var rl = readline.createInterface();
 
+// Get the message to sign.
 rl.question('Message: ', function(msg) {
+  // Get the private key.
   rl.question('Private key: ', function(key) {
+    // Compute the signature.
     var privateKey = new bitcore.PrivateKey(key);
     var message = new Message(msg);
     var signature = message.sign(privateKey);
