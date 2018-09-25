fd41181c98523c348d4cd6b4de8cb53a75e3c2b1
t-bast <bastien@stratumn.com>
2018-09-25 16:03:22 +0200

    *: remove Indigo branding

--- agent/test/actions-todo.js
+++ agent/test/actions-todo.js
@@ -1,7 +1,7 @@
-import Agent from '@indigocore/agent';
-import actions from '../lib/actions-todo';
+import Agent from "@stratumn/agent";
+import actions from "../lib/actions-todo";
 
-describe('actions-todo', () => {
+describe("actions-todo", () => {
   // Transform our actions into a process before every test
   let map;
 
@@ -9,75 +9,75 @@ describe('actions-todo', () => {
     map = Agent.processify(actions);
   });
 
-  describe('#init()', () => {
-    it('sets the state and meta correctly', () => {
-      return map.init('TODO').then(link => {
-        link.state.title.should.be.exactly('TODO');
-        link.meta.tags.should.deepEqual(['list']);
+  describe("#init()", () => {
+    it("sets the state and meta correctly", () => {
+      return map.init("TODO").then(link => {
+        link.state.title.should.be.exactly("TODO");
+        link.meta.tags.should.deepEqual(["list"]);
       });
     });
   });
 
-  describe('#addItem()', () => {
-    it('updates the state and meta correctly', () => {
+  describe("#addItem()", () => {
+    it("updates the state and meta correctly", () => {
       return map
-        .init('TODO')
+        .init("TODO")
         .then(link => {
-          return map.addItem('Do laundry!');
+          return map.addItem("Do laundry!");
         })
         .then(link => {
           link.state.should.deepEqual({
-            description: 'Do laundry!'
+            description: "Do laundry!"
           });
-          link.meta.tags.should.deepEqual(['item']);
+          link.meta.tags.should.deepEqual(["item"]);
         });
     });
 
-    it('must append a list segment', () => {
+    it("must append a list segment", () => {
       return map
-        .init('TODO!')
+        .init("TODO!")
         .then(link => {
-          return map.addItem('Do laundry!');
+          return map.addItem("Do laundry!");
         })
         .then(link => {
-          return map.addItem('Do laundry again!');
+          return map.addItem("Do laundry again!");
         })
         .then(link => {
-          throw new Error('link should not have been created');
+          throw new Error("link should not have been created");
         })
         .catch(err => {
-          err.message.should.be.exactly('not a list');
+          err.message.should.be.exactly("not a list");
         });
     });
   });
 
-  describe('#completeItem()', () => {
-    it('updates the state and meta correctly', () => {
+  describe("#completeItem()", () => {
+    it("updates the state and meta correctly", () => {
       return map
-        .init('TODO')
+        .init("TODO")
         .then(link => {
-          return map.addItem('Do laundry!');
+          return map.addItem("Do laundry!");
         })
         .then(link => {
           return map.completeItem();
         })
         .then(link => {
           link.state.should.deepEqual({});
-          link.meta.tags.should.deepEqual(['completion']);
+          link.meta.tags.should.deepEqual(["completion"]);
         });
     });
 
-    it('must append an item segment', () => {
+    it("must append an item segment", () => {
       return map
-        .init('TODO!')
+        .init("TODO!")
         .then(link => {
           return map.completeItem();
         })
         .then(link => {
-          throw new Error('link should not have been created');
+          throw new Error("link should not have been created");
         })
         .catch(err => {
-          err.message.should.be.exactly('not an item');
+          err.message.should.be.exactly("not an item");
         });
     });
   });
